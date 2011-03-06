a13bdc2da3af5dc4c1436ad20ab1a0c2b38e1b17
Bram Avontuur <bram@avontuur.org>
2011-03-06 14:35:38 +0100

    fix installation of keychar map
    Change-Id: If8597362c00a56c70236b08e22c2212dacf4f7d1

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,6 +1,6 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)/prebuilt
 
 # Install custom keymap
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := prebuilt/tuttle2.kcm
+LOCAL_SRC_FILES := tuttle2.kcm
 include $(BUILD_KEY_CHAR_MAP)
